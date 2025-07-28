382a0b937c02089f7218ca7caa0433c6d7e70bbb
VirgCuts <cvcuts@iastate.edu>
2025-07-28 14:42:58 -0500

    Laptop now connects

--- PokeAuction.API/obj/Debug/net9.0/PokeAuction.API.AssemblyInfo.cs
+++ PokeAuction.API/obj/Debug/net9.0/PokeAuction.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeAuction.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99cd065a8586c6cfba0a0c2649558317e939b0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+336fdd3e7a130a70047a59d2b18965a54693be1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeAuction.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeAuction.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
